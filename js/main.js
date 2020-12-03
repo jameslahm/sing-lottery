1dfaad41b4cea5d6ae724337b48cc33591ca9db6
wangao <1366463855@qq.com>
2020-12-03 10:39:25 +0800

    feat: init

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var $display = $('.display'),
+var $displayList = $('.display'),
 	timer,
 	coord = ['t', 'rt', 'lt', 'b', 'rb', 'lb', 'l', 'r'];
+mode = 1;
 
-
-function display(n, c) {
+function display(n, c, $display) {
 	var $temp, pos;
 	if (n) {
 		$display.text(n);
@@ -26,8 +26,15 @@ function display(n, c) {
 	}
 }
 
-const UpBound = 450;
+function displayList(nList, c) {
+	for (let i = 0; i < 5; i++) {
+		display(nList[i], c, $($displayList[i]))
+	}
+}
+
+const UpBound = 300;
 var n = Math.floor(Math.random() * UpBound) + 1;
+var nList = Array(5).fill(n);
 var work, scroll;
 var paused = true;
 var selected = [];
@@ -35,15 +42,31 @@ var selected = [];
 function calcPrimes() {
 	var c, p;
 	clearInterval(work);
-	work = setInterval(function () {
-		c = Math.floor(Math.random() * 6);
-		display(n, c);
+	if (mode == 1) {
+		work = setInterval(function () {
+			c = Math.floor(Math.random() * 6);
+			displayList(nList, c)
 
-		n = Math.floor(Math.random() * UpBound) + 1;
-		while (selected.indexOf(n) != -1) {
+			for (let i = 0; i < 5; i++) {
+				n = Math.floor(Math.random() * UpBound) + 1;
+				while (selected.indexOf(n) != -1 && nList.indexOf(n) != -1) {
+					n = Math.floor(Math.random() * UpBound) + 1;
+				}
+				nList[i] = n;
+			}
+		}, 75);
+	}
+	else {
+		work = setInterval(function () {
+			c = Math.floor(Math.random() * 6);
+			display(n, c, $($displayList[2]))
+
 			n = Math.floor(Math.random() * UpBound) + 1;
-		}
-	}, 75);
+			while (selected.indexOf(n) != -1) {
+				n = Math.floor(Math.random() * UpBound) + 1;
+			}
+		}, 75);
+	}
 }
 
 function pauseWork() {
@@ -67,7 +90,8 @@ function pauseScroll() {
 
 document.onkeyup = function (e) {
 	var code = e.charCode || e.keyCode;
-	if (code == 13) {
+	console.log(code)
+	if (code === 13) {
 		if (paused) {
 			if (selected.length == UpBound) {
 				alert('所有编号都已选出过，请刷新页面重新开始！');
@@ -81,5 +105,13 @@ document.onkeyup = function (e) {
 			selected.push(n);
 		}
 		paused = !paused;
+	}
+	if (code === 32) {
+		if (mode === 1) {
+			mode = 2;
+		}
+		else {
+			mode = 1;
+		}
 	}
 };
